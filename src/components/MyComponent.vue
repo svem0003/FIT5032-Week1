7d97ffaaf2f1dec8672178f0311b2537b1e46e87
svem0003 <svem0003@student.monash.edu>
2025-08-08 17:17:32 +1000

    Commit for full Week1 E Folio Tasks

---
+++ src/components/MyComponent.vue
@@ -0,0 +1,42 @@
+<script setup>
+defineProps({
+  msg: {
+    type: String,
+    required: false,
+  },
+})
+</script>
+
+<template>
+  <div class="testing">
+    <h1 class="green">{{ msg }}</h1>
+    <h3>
+      My custom testing component.
+    </h3>
+  </div>
+</template>
+
+<style scoped>
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.testing h1,
+.testing h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .testing h1,
+  .testing h3 {
+    text-align: left;
+  }
+}
+</style>
